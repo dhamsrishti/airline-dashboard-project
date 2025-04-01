ac4497dd320b49eedf333cb47696bcdbf85ef132
dhamsrishti <168593602+dhamsrishti@users.noreply.github.com>
2025-04-01 16:53:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="airline-dashboard-project",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
-    package_dir={'': '.'},
+    package_data={
+        '': ['*.yaml', '*.sql', '*.csv'],
+    },
+    include_package_data=True,
     install_requires=[
         "google-cloud-storage",
         "google-cloud-bigquery",
